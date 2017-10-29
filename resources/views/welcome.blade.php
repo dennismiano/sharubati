4d809dae0c0cc59a042d0f525c00473d271c5fe8
dennismiano <dennismiano64@gmail.com>
2017-10-29 01:48:21 -0700

    adding updated footer to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -233,48 +233,75 @@
             </div>
         </div>
 
-        <div class="row footer-section">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="footerform-container">
-                    
-                    <div class="col-md-6 form-container">
-                                <div class="row form-header">
-                                    <h2 class="form-title sharubati-title white">Still got a question?</h2>
+
+
+<div class="row subscribe">
+
+    <div class="container">
+        <div class="col-md-10 col-md-offset-1">
+            <h3 class="white align-center subcriber-statement sharubati-title">Get marketing insights, tips on how to grow your company brand, our informed news letters and much much more right into your inbox.</h3>
+
+                <div class="subscriber-form">
+                    <form class="">
+                        <input type="text" class="form-control no-border subcriber-email" id="budget" placeholder="your email">
+                        <button type="submit" class="form-button pull-right"><b>Subscribe</b><i class="fa fa-arrow-right"></i></button>
+                    </form>
+                </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="row footer">
+    <div class="container">
+        <div class="col-md-4">
+            <h1 class="sharubati-title">Who We Are</h1>
+            <p class="sharubati-text footer-text">Sharubati Technologies is a tech firm that was set up to offer integrated digital solutions to our customers. From design, print, brand origination, UX/UI, web, software development,  digital strategy and marketing, data handling and analytics. 
+            Our  view is most of these services are related and by having them executed in one firm allows for a very informed concept and consistency in execution.
+            </p>
+
+            <h4 class="sharubati-text"><b>Sharubati Technologies Ltd.</b></h4>
+        </div>
+
+        <div class="col-md-4">
+            <h1 class="sharubati-title">We Are Social</h1>
+            <p class="sharubati-text footer-text">Every so often we share our work experience with our followers on social media pages and encourage them to do the same.</p>
+        </div>
+
+        <div class="col-md-4">
+                            <div class="row form-header">
+                                    <h2 class="form-title sharubati-title white">Contact us</h2>
                                 </div>
 
                                 <div class="row form-body">
                                      <form>
+                                      
                                       <div class="form-group">
                                        <!-- <label for="project-category">What is your project about?</label>-->
-                                        <input type="text" class="form-control no-border" id="project-category" placeholder="Your email">
+                                        <input type="text" class="form-control no-border" id="" placeholder="name">
+                                      </div>
+
+                                      <div class="form-group">
+                                       <!-- <label for="project-category">What is your project about?</label>-->
+                                        <input type="text" class="form-control no-border" id="" placeholder="email">
                                       </div>
                                       
                                       <div class="form-group">
                                        <!-- <label for="project-category">What is your project about?</label>-->
-                                        <input type="text" class="form-control no-border" id="budget" placeholder="Your phone number">
+                                        <input type="text" class="form-control no-border" id="budget" placeholder="phone no.">
                                       </div>
                                       
                                       <div class="form-group">
                                        <!-- <label for="project-category">What is your project about?</label>-->
-                                         <textarea class="no-border form-control" rows="3" id="message" name="message" placeholder="Your message"></textarea>
+                                        <textarea class="form-control no-border" cols="3">Message</textarea>
                                       </div>
 
                                       <button type="submit" class="form-button pull-right"><b>A few more details</b><i class="fa fa-arrow-right"></i></button>
                                     </form>
-                                </div>
-                    </div>
-
-
-                    <div class="col-md-6">
-                        <h1 class="sharubati-title">We are social</h1>
-                        <img class="border-bar img-responsive" src = "{{ URL::asset('/images/syde-images/border-bar.png')}}" alt = "computer with design elements">
-                        <p class="sharubati-text">Every now and then we share valuable tips and insights sorrounding our work. We think it might be helpful to your business as well. So like our social media pages and we’ll keep you in the know.</p>
-                    </div>
-
-
-                </div>
-            </div>
+                                </div>  
         </div>
+    </div>
+    
 
         
 		
